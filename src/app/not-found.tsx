d05720011d45220137015da6b30898932111ee51
NBN7 <nqchossj@gmail.com>
2024-11-21 22:32:51 -0300

    add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import { Container } from "@/components/general";
+
+export default function NotFound() {
+  return (
+    <Container
+      container="min-h-screen"
+      className="flex flex-col items-center justify-center gap-4"
+    >
+      <h1 className="font-semibold text-8xl">¡OOPS!</h1>
+      <p className="text-zinc-500 text-lg">Página no encontrada.</p>
+    </Container>
+  );
+}
