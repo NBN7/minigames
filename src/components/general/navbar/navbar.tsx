9ade141c926ff30837853d8c1bbcc5c6f4bcd37f
NBN7 <nqchossj@gmail.com>
2024-11-24 02:31:00 -0300

    update navbar

--- src/components/general/navbar/navbar.tsx
+++ src/components/general/navbar/navbar.tsx
@@ -5,19 +5,21 @@ import { IoLogoGithub } from "react-icons/io";
 
 export const Navbar = () => {
   return (
-    <nav className="w-full flex items-center justify-between gap-4 sticky top-0 p-4 z-50">
-      <Link className="hover:scale-110 transition-all" href="/">
-        <Gamepad2Icon />
-      </Link>
-      <SearchMinigame />
-      <Link
-        href="https://github.com/NBN7/minigames"
-        target="_blank"
-        rel="noreferrer"
-        className="hover:scale-110 transition-all"
-      >
-        <IoLogoGithub className="size-6" />
-      </Link>
+    <nav className="w-full flex items-center justify-center sticky top-0 p-4 z-50 bg-white">
+      <div className="w-full sm:w-[1000px] flex items-center justify-between gap-4">
+        <Link className="hover:scale-110 transition-all" href="/">
+          <Gamepad2Icon />
+        </Link>
+        <SearchMinigame />
+        <Link
+          href="https://github.com/NBN7/minigames"
+          target="_blank"
+          rel="noreferrer"
+          className="hover:scale-110 transition-all"
+        >
+          <IoLogoGithub className="size-6" />
+        </Link>
+      </div>
     </nav>
   );
 };
